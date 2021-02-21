2a004d8b9a7512a815eee3459c008c6cdd4befb1
Taisuke Miyazaki <imishinist@gmail.com>
2021-02-22 02:34:03 +0900

    add buster

--- build.sh
+++ build.sh
@@ -3,8 +3,17 @@
 tag="19.21"
 latest=$tag
 
-docker build ./${tag}/ -t imishinist/dlib:${tag}
+default_variant="alpine3.13"
+docker build ./${tag}/${default_variant} -t imishinist/dlib:${tag}
 docker push imishinist/dlib:${tag}
 
-docker build ./${latest}/ -t imishinist/dlib
+variant="alpine3.13"
+docker build ./${tag}/${variant} -t imishinist/dlib:${tag}-${variant}
+docker push imishinist/dlib:${tag}-${variant}
+
+variant="buster"
+docker build ./${tag}/${variant} -t imishinist/dlib:${tag}-${variant}
+docker push imishinist/dlib:${tag}-${variant}
+
+docker build ./${latest}/${default_variant} -t imishinist/dlib
 docker push imishinist/dlib
