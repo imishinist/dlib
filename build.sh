7cb2b352bf625efd4191d24c22818f1885a51956
Taisuke Miyazaki <imishinist@gmail.com>
2021-02-21 22:22:23 +0900

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 tag="19.21"
+latest=$tag
 
 docker build ./${tag}/ -t imishinist/dlib:${tag}
 docker push imishinist/dlib:${tag}
+
+docker build ./${latest}/ -t imishinist/dlib
+docker push imishinist/dlib
